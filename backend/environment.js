e39594240691f1051256999f69ed394ea67aa3d1
dhavalv3rma <dhavalverma@live.in>
2024-12-19 16:14:17 +0530

    Update environment.js

--- backend/environment.js
+++ backend/environment.js
@@ -1,5 +1,5 @@
 const environment = {
     mongodbURL: 'mongodb+srv://root:rootuser@mern-book-store.wlma5.mongodb.net/?retryWrites=true&w=majority&appName=MERN-Book-Store',
-    port: 3000
+    port: 10000
 }
 export default environment
