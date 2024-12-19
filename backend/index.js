09c7824c9d8c1a7fcb1f6432f14c49419fa870ee
Dhaval Verma <dhavalverma@live.in>
2024-12-19 15:55:47 +0530

    env fix

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,5 @@
 import express from "express";
-import environment from "./environment.js";
+import environment from "./environment.ts";
 import mongoose from "mongoose";
 import booksRoute from "./routes/booksRoute.js";
 
